e20d29f99a825c6600c5b83ea81d329d9a1bc944
Bradley Owens <bradowens17@hotmail.com>
2022-10-14 20:16:22 +1100

    fix styling bug by adding classes to the editSkills components inputs

--- src/Components/AccountEdit/Skills/EditSkills.js
+++ src/Components/AccountEdit/Skills/EditSkills.js
@@ -1,14 +1,36 @@
+import { useReducer } from "react";
 import Modal from "../../UI/Modal/Modal";
 import styles from "../Edit.module.css";
 
 const EditSkills = (props) => {
+  const frameworkReducer = (state, action) => {
+    if (action.type === "USER-INPUT") return { value: action.val };
+  };
+
+  const [frameworkState, dispatchFramework] = useReducer(frameworkReducer, {
+    value: "",
+  });
+
+  const frameworkHandler = (e) => {
+    dispatchFramework({
+      val: e.target.value,
+    });
+  };
+
+  //////////////////////////////////////////////////////////////////
+
   return (
     <Modal onClose={props.onClose}>
       <h1>Skills and Experience</h1>
       <p>Optional</p>
       <form>
-        <input type="text" placeholder="Frameworks"></input>
-        <select placeholder="Education">
+        <input
+          className={styles.input}
+          type="text"
+          onChange={frameworkHandler}
+          placeholder="Frameworks"
+        ></input>
+        <select className={styles.input} placeholder="Education">
           <option value="" disabled selected>
             How do you know how to code?
           </option>
@@ -19,12 +41,15 @@ const EditSkills = (props) => {
           <option value="none">Looking for right path</option>
         </select>
         <input
+          className={styles.input}
           type="number"
           placeholder="How many years experience (years)"
         ></input>
         <div className={styles.buttons}>
-          <button>Submit</button>
-          <button onClick={props.onClose}>Cancel</button>
+          <button className={styles.button}>Submit</button>
+          <button onClick={props.onClose} className={styles.button}>
+            Cancel
+          </button>
         </div>
       </form>
     </Modal>
--- src/Pages/Account/AccountEdit.js
+++ src/Pages/Account/AccountEdit.js
@@ -21,20 +21,3 @@ const AccountEdit = () => {
 };
 
 export default AccountEdit;
-
-{
-  /* <h1 className={styles.title}>Edit Profile</h1>
-<div className={styles["modal-flex"]}>
-  <div className={styles["account-modal"]}>
-  
-  </div>
-  <div className={styles["personal-modal"]}>
-   
-      <input type="text" placeholder="LinkedIN"></input>
-      <input type="text" placeholder="Github"></input>
-      <input type="text" placeholder="Portfolio Website"></input>
-      <button>Submit</button>
-    </form>
-  </div>
-</div> */
-}
