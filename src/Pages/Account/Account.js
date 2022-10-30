b0b78cdb3afb45aeb9526560418a8320fa7f008d
Bradley Owens <bradowens17@hotmail.com>
2022-10-30 14:24:36 +1100

    add input for user to choose a display picture and render when chosen, if user does not choose one a rendered non profile picture img will be displayed

--- src/Pages/Account/Account.js
+++ src/Pages/Account/Account.js
@@ -1,13 +1,22 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import { useSelector } from "react-redux";
 import Connections from "../../Components/Account/Connections";
 import styles from "./Account.module.css";
 import AccountInformation from "./AccountInformation";
+import displayImg from "../../Components/IMG/noProfile.png";
 
 const Account = () => {
   const userName = useSelector(
     (state) => state.authentication.loggedInUser.info.name
   );
+
+  const [image, setImage] = useState(displayImg);
+
+  const onImageChange = (event) => {
+    if (event.target.files && event.target.files[0]) {
+      setImage(URL.createObjectURL(event.target.files[0]));
+    }
+  };
   return (
     <Fragment>
       <div className={styles["account-container"]}>
@@ -17,6 +26,11 @@ const Account = () => {
             Edit your profile to show more information about yourself to other
             members
           </p>
+          <div>
+            <h3>Change your profile picture</h3>
+            <input type="file" onChange={onImageChange} className="filetype" />
+            <img src={image} className={styles.picture} alt="preview image" />
+          </div>
         </div>
         <Connections />
       </div>
